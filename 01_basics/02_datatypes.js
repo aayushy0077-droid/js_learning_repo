0c50955a50d38d338c8b930e1c86f3c4b60b4ffa
Aayush Yadav <139547466+Aayushy7777@users.noreply.github.com>
2025-12-20 08:33:51 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code as new version
+
+// alert(3+3) // we are using nodejs not browser
+
+let name = "aayush"
+let age = 20
+let isLoggedin = true
+let state;
+
+// number => 2 to power 53
+//bigint 
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol => unique
+
+console.log(typeof null);  // object is a type of null
+console.log(typeof undefined);  // undefined is type of undefined only
