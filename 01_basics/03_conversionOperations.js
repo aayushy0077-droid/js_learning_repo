ca5aae06c29d4839797118f8cdd49b8743d6dfbd
Aayush Yadav <139547466+Aayushy7777@users.noreply.github.com>
2025-12-24 08:09:56 +0000

    lecture 7

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,26 +1,66 @@
-let score = true
+// let score = true
 
-console.log(typeof (score));
-console.log(typeof score);
+// console.log(typeof (score));
+// console.log(typeof score);
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" =>  NaN
 // true => 1 / false => 0
 
-let isLoggedIn = "abc"
-let BooleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(BooleanIsLoggedIn);
+// let isLoggedIn = "abc"
+// let BooleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(BooleanIsLoggedIn);
 
 // 1 => true / 0 => false
 // " " => false
 // "abc" => true
 
-let SomeNumber = 33
+// let SomeNumber = 33
 
-let StringNumber = String(SomeNumber);
-console.log(StringNumber);
-console.log(typeof StringNumber);
+// let StringNumber = String(SomeNumber);
+// console.log(StringNumber);
+// console.log(typeof StringNumber);
+
+/***********************************/
+// let value = 3
+// let negValue = -value
+// console.log(negValue)
+
+// console.log(2**3)
+
+// let str1 = "aayush"
+// let str2 = "yadav"
+
+// console.log( str1 + str2);
+
+// console.log("1" + 2)
+// console.log("1" + "2")
+// console.log(1 +2 + "2")
+
+// console.log(   (3+4) * 5 % 4);
+
+
+// console.log(+"");
+
+// let num1,num2, num3
+
+// num1 = num2 = num3 = 2+2
+// let gameCounter = 100
+// ++gameCounter;
+// console.log(gameCounter);
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
