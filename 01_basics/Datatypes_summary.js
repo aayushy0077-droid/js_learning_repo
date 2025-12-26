8bd680657d998a964221833dc413d396ff74be77
aayushy0077-droid <aayush.y0077@gmail.com>
2025-12-26 08:01:57 +0000

    Dataypes_summary

---
+++ 01_basics/Datatypes_summary.js
@@ -0,0 +1,39 @@
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+// const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+
+let myObj ={
+    name: "Aayush",
+    age:  22,
+}
+
+const myfunction = function() {
+    console.log("Hello world");
+
+    
+}
+// console.log(typeof outsideTemp);
+// console.log(typeof scoreValue);
+console.log(typeof heros);
